73b9c6976cfdf86200b547a381d54ecbdb0f3466
SURAJ-PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2022-07-07 14:25:18 +0530

    Add files via upload

---
+++ Transpose_Matrix.java
@@ -0,0 +1,24 @@
+public class Transpose_Matrix {
+    public static void printTraspose(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr[i].length;j++){
+                //swap (a[i][j],a[j][i])
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        for(int i=0;i< arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3,4} ,{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        printTraspose(arr);
+    }
+}
